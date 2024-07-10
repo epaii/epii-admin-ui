1ba6eb9a0e57d0f4b8b9075209e9496b541f2026
Your Name <you@example.com>
2024-07-10 09:52:29 +0800

    rlr

--- src/app/view/index/xxAdmin.php
+++ src/app/view/index/xxAdmin.php
@@ -16,15 +16,15 @@ $html = replaceFirst("<script", $injectDataString . "<script", $html);
 
 $html = str_replace(["src=\"/", "src=\"./"], "src=\"" . $_ui_["xxAdmin_static_url_pre"], $html);
 $html = str_replace(["href=\"/", "href=\"./"], "href=\"" . $_ui_["xxAdmin_static_url_pre"], $html); 
-$html = replaceFirst("<head>",  "<head><link rel=\"stylesheet\" href=\"https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/css/font-awesome.min.css\"><link rel=\"stylesheet\" href=\"https://epiiadmin-js.chinacloudsites.cn/css/ionicons.min.css\">", $html);
+$html = replaceFirst("<head>",  "<head><link rel=\"stylesheet\" href=\"{$_ui_["static_url_pre"]}js/plugins/font-awesome/css/font-awesome.min.css\"><link rel=\"stylesheet\" href=\"{$_ui_["static_url_pre"]}css/ionicons.min.css\">", $html);
 
 
 $fontFace = <<<XLX
 
 @font-face {
     font-family: 'FontAwesome';
-    src: url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.eot?v=4.7.0');
-    src: url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'), url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.woff2?v=4.7.0') format('woff2'), url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.woff?v=4.7.0') format('woff'), url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.ttf?v=4.7.0') format('truetype'), url('https://epiiadmin-js.chinacloudsites.cn/js/plugins/font-awesome/fonts//fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg');
+    src: url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.eot?v=4.7.0');
+    src: url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'), url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.woff2?v=4.7.0') format('woff2'), url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.woff?v=4.7.0') format('woff'), url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.ttf?v=4.7.0') format('truetype'), url('{$_ui_["fontawesome_fonts_url_pre"]}fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg');
     font-weight: normal;
     font-style: normal;
 }
